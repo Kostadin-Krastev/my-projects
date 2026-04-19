6458e2638bc47fa86c23b5103b6326b7a14c5596
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-04-19 17:38:02 +0300

    add comment

--- 01-expanding-cards/my-if-else-solution.js
+++ 01-expanding-cards/my-if-else-solution.js
@@ -7,7 +7,7 @@ panelCard.forEach(function (card) {
   card.addEventListener('click', function () {
     const activePanel = document.querySelector('.active');
 
-    // Removing active class
+    // Removing active class when activePanel variable contains value (contains the active class, and is not null or undefined (empty))
     if (activePanel) {
       activePanel.classList.remove('active');
     }
