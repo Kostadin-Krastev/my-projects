68f501385baeac5d95458c57a3aec7d14bb5922c
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-04-19 18:20:59 +0300

    refactoring the code

--- 01-expanding-cards/my-if-else-solution.js
+++ 01-expanding-cards/my-if-else-solution.js
@@ -1,15 +1,16 @@
 'use strict';
 // Variable for the panels
-const panelCard = document.querySelectorAll('.panel');
+const panelCards = document.querySelectorAll('.panel');
 
 // Solution with looping all the panels and using "if else" to remove or add active class to the clicked panel
-panelCard.forEach(function (card) {
+panelCards.forEach(function (card) {
   card.addEventListener('click', function () {
-    const activePanel = document.querySelector('.active');
+    // Storing the active card
+    const activeCard = document.querySelector('.active');
 
     // Removing active class when activePanel variable contains value (contains the active class, and is not null or undefined (empty))
-    if (activePanel) {
-      activePanel.classList.remove('active');
+    if (activeCard) {
+      activeCard.classList.remove('active');
     }
 
     // Add active class
