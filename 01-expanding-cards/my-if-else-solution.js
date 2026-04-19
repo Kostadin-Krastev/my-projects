6dde08b5712c420414c979c345aeabdf76178576
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2026-04-19 17:33:58 +0300

    my solution using forEach loop and if else statemant inside the loop

---
+++ 01-expanding-cards/my-if-else-solution.js
@@ -0,0 +1,18 @@
+'use strict';
+// Variable for the panels
+const panelCard = document.querySelectorAll('.panel');
+
+// Solution with looping all the panels and using "if else" to remove or add active class to the clicked panel
+panelCard.forEach(function (card) {
+  card.addEventListener('click', function () {
+    const activePanel = document.querySelector('.active');
+
+    // Removing active class
+    if (activePanel) {
+      activePanel.classList.remove('active');
+    }
+
+    // Add active class
+    card.classList.add('active');
+  });
+});
